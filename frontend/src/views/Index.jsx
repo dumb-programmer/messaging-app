c20103d9e4ef0e3fe42731ca883808b697e26fe0
Asad Khan <asadkhan6192@gmail.com>
2024-05-06 23:00:54 +0500

    Make logout into a button, and implement logout functionality

--- frontend/src/views/Index.jsx
+++ frontend/src/views/Index.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Navigate, Outlet } from "react-router-dom";
+import { NavLink, Navigate, Outlet, useNavigate } from "react-router-dom";
 import ChatIcon from "../icons/ChatIcon";
 import LogoutIcon from "../icons/LogoutIcon";
 import PeoplesIcon from "../icons/PeoplesIcon";
@@ -10,7 +10,8 @@ import "../styles/Index.css";
 import InboxIcon from "../icons/InboxIcon";
 
 const Index = () => {
-  const { auth } = useAuthContext();
+  const { auth, updateAuth } = useAuthContext();
+  const navigate = useNavigate();
 
   if (!auth) {
     return <Navigate to="/login" />;
@@ -23,58 +24,68 @@ const Index = () => {
   return (
     <SocketProvider>
       <main>
-        <div className="tabs">
-          <nav>
-            <ul>
-              <li className="tab">
-                <NavLink to="/">
-                  <ChatIcon size={23} color="black" strokeWidth={1.5} />
-                  Chats
-                </NavLink>
-              </li>
-              <li className="tab">
-                <NavLink to="/people">
-                  <PeoplesIcon size={23} color="black" strokeWidth={1.5} />
-                  People
-                </NavLink>
-              </li>
-              <li className="tab">
-                <NavLink to="/requests">
-                  <InboxIcon size={23} color="black" strokeWidth={1.5} />
-                  Requests
-                </NavLink>
-              </li>
-              <li className="tab">
-                <NavLink to="/settings">
-                  <SettingsIcon size={23} color="black" strokeWidth={1.5} />
-                  Settings
-                </NavLink>
-              </li>
-              <li className="tab">
-                <NavLink to="/logout">
-                  <LogoutIcon size={23} color="black" strokeWidth={1.5} />
-                  Logout
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-          <div className="account-info">
-            <img
-              src={`http://localhost:3000/${avatar}`}
-              className="avatar"
-              style={{
-                height: 60,
-                width: 60,
-                borderRadius: "50%",
-                objectFit: "cover",
-              }}
-            />
-            <div className="flex flex-column gap-sm">
-              <h4>{`${firstName} ${lastName}`}</h4>
-              <p className="text-sm" style={{ color: "#607274" }}>@{username}</p>
+        <aside style={{ position: "relative" }}>
+          <div className="tabs">
+            <nav>
+              <ul>
+                <li className="tab">
+                  <NavLink to="/">
+                    <ChatIcon size={23} color="black" strokeWidth={1.5} />
+                    Chats
+                  </NavLink>
+                </li>
+                <li className="tab">
+                  <NavLink to="/people">
+                    <PeoplesIcon size={23} color="black" strokeWidth={1.5} />
+                    People
+                  </NavLink>
+                </li>
+                <li className="tab">
+                  <NavLink to="/requests">
+                    <InboxIcon size={23} color="black" strokeWidth={1.5} />
+                    Requests
+                  </NavLink>
+                </li>
+                <li className="tab">
+                  <NavLink to="/settings">
+                    <SettingsIcon size={23} color="black" strokeWidth={1.5} />
+                    Settings
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className="account-info">
+              <img
+                src={`http://localhost:3000/${avatar}`}
+                className="avatar"
+                style={{
+                  height: 60,
+                  width: 60,
+                  borderRadius: "50%",
+                  objectFit: "cover",
+                }}
+              />
+              <div className="flex align-center gap-md">
+                <div className="flex flex-column gap-sm">
+                  <h4>{`${firstName} ${lastName}`}</h4>
+                  <p className="text-sm" style={{ color: "#607274" }}>
+                    @{username}
+                  </p>
+                </div>
+                <button
+                  aria-label="logout"
+                  className="btn"
+                  onClick={() => {
+                    updateAuth(() => null);
+                    navigate("/login");
+                  }}
+                >
+                  <LogoutIcon size={20} strokeWidth={1.5} color="black" />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        </aside>
         <div className="view-container">
           <ToastsContainer>
             <Outlet />
